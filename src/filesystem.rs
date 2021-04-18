175d771f8e1f4234a37b8a962708043c2642f71f
druskus20 <druskus20@gmail.com>
2021-04-19 00:29:25 +0200

    q

--- src/filesystem.rs
+++
@@ -1,7 +0,0 @@
-/*
-   Idea is:
-       - There's a pile of notes unorganized in one directory
-       - There are separate directories for each tag, that contain symlinks to notes in the pile directory
-
-
-*/
